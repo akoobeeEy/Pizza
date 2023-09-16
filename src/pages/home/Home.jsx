f2e9ecdb4cb0fc78436da31d37c31397fb1aa96a
akoobeeEy <119799055+akoobeeEy@users.noreply.github.com>
2023-09-16 12:18:00 +0500

    gg

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -51,6 +51,110 @@ const Home = () => {
             ))}
         </div>
       </section>
+      <section id="snack" className="py-20">
+        <div className="flex justify-between mb-5">
+          <h1 className="text-3xl font-bold">Закуски</h1>
+
+          <select name="price" id="price">
+            <option value="">All Poducts</option>
+            <option value="New">New</option>
+            <option value="Hot">Hot</option>
+          </select>
+        </div>
+        <div className="grid grid-cols-4 gap-10">
+          {products
+            .filter((el) => el.category === "Закуски")
+            .map((pr) => (
+              <div key={pr.id} className="">
+                <ProductCard {...pr} />
+              </div>
+            ))}
+        </div>
+      </section>
+      <section id="desert" className="py-20">
+        <div className="flex justify-between mb-5">
+          <h1 className="text-3xl font-bold">Десерты</h1>
+
+          <select name="price" id="price">
+            <option value="">All Poducts</option>
+            <option value="New">New</option>
+            <option value="Hot">Hot</option>
+          </select>
+        </div>
+        <div className="grid grid-cols-4 gap-10">
+          {products
+            .filter((el) => el.category === "Десерты")
+            .map((pr) => (
+              <div key={pr.id} className="">
+                <ProductCard {...pr} />
+              </div>
+            ))}
+        </div>
+      </section>
+      <section id="drink" className="py-20">
+        <div className="flex justify-between mb-5">
+          <h1 className="text-3xl font-bold">Напитки</h1>
+
+          <select name="price" id="price">
+            <option value="">All Poducts</option>
+            <option value="New">New</option>
+            <option value="Hot">Hot</option>
+          </select>
+        </div>
+        <div className="grid grid-cols-4 gap-10">
+          {products
+            .filter((el) => el.category === "Напитки")
+            .map((pr) => (
+              <div key={pr.id} className="">
+                <ProductCard {...pr} />
+              </div>
+            ))}
+        </div>
+      </section>
+      <section id="sauce" className="py-20">
+        <div className="flex justify-between mb-5">
+          <h1 className="text-3xl font-bold">Соусы</h1>
+
+          <select name="price" id="price">
+            <option value="">All Poducts</option>
+            <option value="New">New</option>
+            <option value="Hot">Hot</option>
+          </select>
+        </div>
+        <div className="grid grid-cols-4 gap-10">
+          {products
+            .filter((el) => el.category === "Соусы")
+            .map((pr) => (
+              <div key={pr.id} className="">
+                <ProductCard {...pr} />
+              </div>
+            ))}
+        </div>
+      </section>
+      
+   
+      <section id="combo" className="py-20">
+        <div className="flex justify-between mb-5">
+          <h1 className="text-3xl font-bold">Комбо</h1>
+
+          <select name="price" id="price">
+            <option value="">All Poducts</option>
+            <option value="New">New</option>
+            <option value="Hot">Hot</option>
+          </select>
+        </div>
+        <div className="grid grid-cols-4 gap-10">
+          {products
+            .filter((el) => el.category === "Комбо")
+            .map((pr) => (
+              <div key={pr.id} className="">
+                <ProductCard {...pr} />
+              </div>
+            ))}
+        </div>
+      </section>
+   
+     
     </div>
   );
 };
